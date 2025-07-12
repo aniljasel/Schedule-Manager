a6f5a3ae544dec623d1bc5f42be54a3aa2b8f017
Anil Kumar <ak1109539@gmail.com>
2025-07-12 19:19:10 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import './index.css';
 import App from './App.jsx';
 
