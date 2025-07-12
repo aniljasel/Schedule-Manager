535c5bca1dd687998c55bbdeedfca853ea4ae5eb
Anil Kumar <ak1109539@gmail.com>
2025-07-12 19:17:54 +0530

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HashRouter>
-      <App />
+      <SpeedInsights>
+        <App />
+      </SpeedInsights>
     </HashRouter>
   </StrictMode>
 );
